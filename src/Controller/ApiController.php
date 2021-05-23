6bcd7fab9c116a91accc50c6ede8893134db4fc1
BruccoleriAlois <46890555+BruccoleriAlois@users.noreply.github.com>
2021-05-23 17:44:01 +0200

    Modification de l'emplacement des RDV
    Ajout de la possibilité de déplacer les rdv déjà présent dans le calendrier et sauvegarde des changement dans la bdd

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Evenement;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use DateTime;
+
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/api", name="api")
+     */
+    public function index(): Response
+    {
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
+
+    /**
+     * @Route("/api/{id}/edit", name="api_event_edit", methods={"PUT"})
+     */
+    public function majEvent(?Evenement $evenement,Request $request)
+    {
+
+        // récupération des données
+        $donnees = json_decode($request->getContent());
+
+        if(
+            isset($donnees->title) && !empty($donnees->title) &&
+            isset($donnees->start) && !empty($donnees->start) &&
+            isset($donnees->description) && !empty($donnees->description) &&
+            isset($donnees->backgroundColor) && !empty($donnees->backgroundColor) &&
+            isset($donnees->borderColor) && !empty($donnees->borderColor) &&
+            isset($donnees->textColor) && !empty($donnees->textColor) 
+        ){
+            //données complètes
+            //initialisation d'un code
+            $code = 200;
+
+            //vérification si l'id existe
+            if(!$evenement){
+                //création d'un evenement
+                $evenement = new Evenement;
+                //changement du code
+                $code = 201;
+            }
+            //on donne les donnée
+            $evenement->setTitre($donnees->title);
+            $evenement->setDescription($donnees->description);
+            $evenement->setDebut(new DateTime($donnees->start));
+            if($donnees->allDay){
+                $evenement->setFin(new DateTime($donnees->start));
+            }else{
+                $evenement->setFin(new DateTime($donnees->end));
+            }
+            $evenement->setJourneeComplete($donnees->allDay);
+            $evenement->setCouleurFond($donnees->backgroundColor);
+            $evenement->setCouleurBordure($donnees->borderColor);
+            $evenement->setCouleurTexte($donnees->textColor);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($evenement);
+            $em->flush();
+
+            //on retrourne le code
+            return new Response('Ok', $code);
+        }else{
+            //données incomplètes
+            return new Response('Données incomplètes', 404);
+        }
+
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
+}
