3f1bcc2a654da04c4b4dc7a7436b1a88b7c99d8e
BruccoleriAlois <46890555+BruccoleriAlois@users.noreply.github.com>
2021-05-23 15:14:44 +0200

    Ajout table evenement

---
+++ src/Form/EvenementType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evenement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvenementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('debut')
+            ->add('fin')
+            ->add('description')
+            ->add('journee_complete')
+            ->add('couleur_fond')
+            ->add('couleur_bordure')
+            ->add('couleur_texte')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Evenement::class,
+        ]);
+    }
+}
