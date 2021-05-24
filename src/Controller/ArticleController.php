aa135debc309b59bc24b5b93699984a011bdd0b7
JUJU <julienhermange@gmail.com>
2021-05-24 15:09:18 +0200

    Feat article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+
 
 class ArticleController extends AbstractController
 { 
@@ -21,12 +26,7 @@ class ArticleController extends AbstractController
 
    private $repository;
 
-   /**public function __construct(ArticleRepository $repository,ObjectManager $em)
-   {
-       $this->repository = $repository;
-       $this->em = $em;
-   }
-   */
+
     /**
      * @Route("/article", name="article")
      */
@@ -35,5 +35,45 @@ class ArticleController extends AbstractController
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
         ]);
+    }
+
+    /**
+     * @Route("/article/new")
+     */
+    public function createAction(Request $request) {
+        $articles = new Article();
+        $form = $this->createFormBuilder($articles)
+            ->add('Titre', TextType::class)
+            ->add('Description', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Valider'])
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $articles = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($articles);
+            $em->flush();
+            echo 'Envoyé';
+        }
+        return $this->render('article/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}")
+     */
+    public function viewAction($id) {
+        $article = $this->getDoctrine()->getRepository(Article::class);
+        $article = $article->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'Aucun article pour l\'id: ' . $id
+            );
+        }
+        return $this->render(
+            'article/view.html.twig',
+            array('article' => $article)
+        );
     }
 }
