5fefaa36ec1c281f3d658f62a3c89b927cac9990
JUJU <julienhermange@gmail.com>
2021-05-24 16:44:20 +0200

    Article fonctionnel

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,6 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
@@ -32,21 +34,26 @@ class ArticleController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
-        ]);
+        $articles = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $articles->findAll();
+        return $this->render('article/index.html.twig', 
+        array('articles' => $articles));
     }
 
     /**
-     * @Route("/article/new")
+     * @IsGranted("ROLE_ADMIN")
+     * @IsGranted("ROLE_BDE")
+     * @Route("/article/ajouter")
      */
     public function createAction(Request $request) {
+
         $articles = new Article();
+
         $form = $this->createFormBuilder($articles)
             ->add('Titre', TextType::class)
             ->add('Description', TextType::class)
-            ->add('save', SubmitType::class, ['label' => 'Valider'])
             ->getForm();
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $articles = $form->getData();
@@ -55,7 +62,7 @@ class ArticleController extends AbstractController
             $em->flush();
             echo 'Envoyé';
         }
-        return $this->render('article/new.html.twig', [
+        return $this->render('article/ajouter.html.twig', [
             'form' => $form->createView(),
         ]);
     }
@@ -72,8 +79,59 @@ class ArticleController extends AbstractController
             );
         }
         return $this->render(
-            'article/view.html.twig',
-            array('article' => $article)
+            'article/voir.html.twig',
+            array('Article' => $article)
+        );
+    }
+
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @IsGranted("ROLE_BDE")
+     * @Route("article/supprimer/{id}" , name="article_delete")
+     */
+    public function deleteAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $article = $this->getDoctrine()->getRepository(Article::class);
+        $article = $article->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'There are no articles with the following id: ' . $id
+            );
+        }
+        $em->remove($article);
+        $em->flush();
+        return $this->redirect($this->generateUrl('article'));
+    }
+
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @IsGranted("ROLE_BDE")
+     * @Route("article/modifier/{id}", name="article_edit")
+     */
+    public function updateAction(Request $request, $id) {
+        $article = $this->getDoctrine()->getRepository(Article::class);
+        $article = $article->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'There are no articles with the following id: ' . $id
+            );
+        }
+        $form = $this->createFormBuilder($article)
+            ->add('titre', TextType::class)
+            ->add('description', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Editer'))
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            $article = $form->getData();
+            $em->flush();
+            return $this->redirect($this->generateUrl('article'));
+        }
+        return $this->render(
+            'article/modifier.html.twig',
+            array('form' => $form->createView())
         );
     }
 }
