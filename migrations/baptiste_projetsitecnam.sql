afe5fae1b1bc5eb42d433e434f9e70b03c71f5af
BruccoleriAlois <46890555+BruccoleriAlois@users.noreply.github.com>
2021-06-02 15:30:36 +0200

    Update baptiste_projetsitecnam.sql

--- migrations/baptiste_projetsitecnam.sql
+++ migrations/baptiste_projetsitecnam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql-baptiste.alwaysdata.net
--- Generation Time: May 26, 2021 at 04:13 PM
+-- Generation Time: Jun 02, 2021 at 03:29 PM
 -- Server version: 10.5.8-MariaDB
 -- PHP Version: 7.4.19
 
@@ -39,8 +39,8 @@ CREATE TABLE `article` (
 --
 
 INSERT INTO `article` (`id`, `titre`, `description`, `created_at`) VALUES
-(3, 'test', 'test', '2021-05-24 13:32:51'),
-(4, 'test2', 'test', '2021-05-24 13:36:14');
+(5, 'Tournoi de jeu vidéo !', 'Bonjour à tous, afin de fêter la fin d\'année nous organisons un tournoi sur SuperSmashBros avec 3 manches gagnantes le 20/06/2021 ! A vos manettes !!', '2021-06-02 09:19:28'),
+(6, 'Sortie bar !', 'Hello, un petit coup a boire ca vous tente ? rendez vous le 15/06/2021 à 19h dans le bar la plage au mourillon !', '2021-06-02 09:26:26');
 
 -- --------------------------------------------------------
 
@@ -54,6 +54,15 @@ CREATE TABLE `competence` (
   `libelle` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `niveau_maitrise` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `competence`
+--
+
+INSERT INTO `competence` (`id`, `cv`, `libelle`, `niveau_maitrise`) VALUES
+(20, 62, 'c++', 1),
+(21, 62, 'java', 1),
+(22, 62, 'css', 3);
 
 -- --------------------------------------------------------
 
@@ -78,7 +87,7 @@ CREATE TABLE `cv` (
 --
 
 INSERT INTO `cv` (`id`, `email`, `adresse`, `telephone`, `date_anniversaire`, `lien_site`, `bio`, `utilisateur`, `public`) VALUES
-(62, 'a', NULL, NULL, '2021-01-01', NULL, 'a', 8, 1);
+(62, 'baptiste@univ.com', 'NR', '0612123434', '1990-11-12', 'baptiste.portfolio.com', 'Bonjour, ...', 8, 1);
 
 -- --------------------------------------------------------
 
@@ -135,7 +144,9 @@ INSERT INTO `evenement` (`id`, `titre`, `debut`, `fin`, `description`, `journee_
 (3, 'RDV', '2021-05-27 10:00:00', '2021-05-27 11:00:00', 'Petite réunion', 0, '#8000ff', '#000000', '#ffffff'),
 (4, 'test', '2021-05-27 03:30:00', '2021-05-28 07:30:00', 'test', 0, '#00c3d1', '#276fce', '#265aba'),
 (5, 'La Journée de la truite', '2021-05-26 00:00:00', '2021-05-27 00:00:00', 'Journée de la truite', 1, '#00ffff', '#ff80ff', '#000000'),
-(6, 'Soirée pyjama', '2021-05-29 19:00:00', '2021-05-30 12:00:00', 'Venez nombreux ! :-)', 0, '#d4a2d7', '#19f00a', '#ff0000');
+(6, 'Soirée pyjama', '2021-05-29 21:30:00', '2021-05-30 14:30:00', 'Venez nombreux ! :-)', 0, '#d4a2d7', '#19f00a', '#ff0000'),
+(7, 'test', '2021-06-04 01:30:00', '2021-06-05 01:30:00', 'test', 0, '#ea0606', '#000000', '#000000'),
+(8, 'test2', '2021-06-03 18:00:00', '2021-06-02 14:40:53', 'test2', 0, '#00ffbf', '#000000', '#000000');
 
 -- --------------------------------------------------------
 
@@ -160,7 +171,13 @@ CREATE TABLE `utilisateur` (
 INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `login`, `pwd`, `role`, `cv`) VALUES
 (8, 'Blanchet', 'Baptiste', 'baptiste', '$2y$13$SgLN1ryui2hcRNefz//.RudgaOwoUUY47ZTQ8MCE2SL9JQq93hwHG', 'abcd', 62),
 (10, 'Hermange', 'Julien', 'Julien', '$2y$13$HYi.Eh6slb8xt/oK6EctvenU3Cwa1I/Jh8W2reMvQhlVy6t/iK4cC', 'abcd', NULL),
-(13, 'brucomachin', 'Aloa', 'Aloa', '$2y$13$NMk8JZbrdEPNS1xmwJFReek0AH8g0SXvjWM45CgxbKJQ9ghvg/4va', 'abcd', NULL);
+(13, 'brucomachin', 'Aloa', 'Aloa', '$2y$13$NMk8JZbrdEPNS1xmwJFReek0AH8g0SXvjWM45CgxbKJQ9ghvg/4va', 'abcd', NULL),
+(14, 'Admin', 'Super', 'superadmin', '$2y$13$ONFmOwLrYmhlr7APMP/dwOZ97pz2sE1yBmzWWh2e74Tqkq2UmFA6G', 'abcd', NULL),
+(15, 'root', 'admin', 'admin', '$2y$13$AGlOeK1PAI.bIRcWnfNEZOa0y1QTPHMlUI277y1GSCSWC9.xmOArS', 'a', NULL),
+(16, 'Intervenant', 'Inter', 'intervenant', '$2y$13$pU1dnjnbW3NzXRWGwN0KneSx3MyGmmQv0jkdzDcQS53bXu/XaNbqa', 'b', NULL),
+(17, 'BDE', 'bde', 'bde', '$2y$13$flp097gJaQijE1en7EzGg.o26OCH0aNfVvpIkU5eHbNeMUd16HDeW', 'c', NULL),
+(18, 'Etudiant', 'etudiant', 'etudiant', '$2y$13$N1UzE0yFDvNOebNPqhe18umjgMWp7B4Ot7iIbpnaCxJB/gm19k/c6', 'd', NULL),
+(19, 'Testnom', 'Testprenom', 'testlog', '$2y$13$4dfXUgwTlBy.gVpuUczKD./7vn455hAZRL2u8xYMFQLKHSvfOyw1e', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -213,13 +230,13 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT for table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `competence`
 --
 ALTER TABLE `competence`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `cv`
@@ -231,13 +248,13 @@ ALTER TABLE `cv`
 -- AUTO_INCREMENT for table `evenement`
 --
 ALTER TABLE `evenement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Constraints for dumped tables
